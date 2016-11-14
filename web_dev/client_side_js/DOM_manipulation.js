88dd38b8123a4c3857d4a33bf6227541cb5e9cbf
Vadim Brand <brandv7@gmail.com>
2016-11-13 19:56:46 -0600

    9.2

--- web_dev/client_side_js/DOM_manipulation.js
+++ web_dev/client_side_js/DOM_manipulation.js
@@ -1,8 +1,22 @@
 //console.log("Here we go");
 
 
+// function roll() {
+// 	if q1 = true
+// 		document.getElementById("q1")
 
+// }
 
+function blueColor() {
+	var q1 = document.getElementById("q1").style.textShadow = "5px 5px 0.5px blue";
+}
 
+function greenColor() {
+	var q2 = document.getElementById("q2").style.textShadow = "5px 5px 0.5px green";
+}
 
-//document.addEventLister("load", function ())
+var q1 = document.getElementById("q1").style.textShadow = "5px 5px 0.5px blue";
+q1.addEventLister("click", blueColor);
+
+var q2 = document.getElementById("q2").style.textShadow = "5px 5px 0.5px green";
+q2.addEventLister("load", greenColor);
